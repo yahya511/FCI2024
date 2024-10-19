c9ee4baf943acabd6f1379a85a38cd5f12656f5d
yahya511 <yehia.449034952@s6m.moe.edu.eg>
2024-10-19 07:59:58 +0300

    Rebuild the project

--- FCIProjects/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ FCIProjects/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e3f925fd3dc8b5fa534cc0bf08a159f6139923")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
